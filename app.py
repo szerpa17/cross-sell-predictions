b338c13aea2a0e0fdb0780dd3fb17099f3770454
szerp <szerp002@gmail.com>
2020-11-05 23:56:04 -0500

    updated the original_testing_data app route

--- app.py
+++ app.py
@@ -26,19 +26,18 @@ def welcome():
     return (
         f"Available Routes:<br/>"
         f"/api/v1.0/testing_data<br/>"
-        # f"/api/v1.0/passengers"
+        f"/api/v1.0/original_testing_data"
     )
 
 
 @app.route("/api/v1.0/testing_data")
-def names():
+def testing_data():
     conn = sqlite3.connect(insurance_info)
 
     # Create our session (link) from Python to the DB
     test_data = conn.execute("SELECT * FROM test_data;")
 
     """Return a list"""
-    # Query all test_data
     test_data_array = []
 
     for row in test_data:
@@ -59,27 +58,32 @@ def names():
     return jsonify(test_data_array)
 
 
-# @app.route("/api/v1.0/passengers")
-# def passengers():
+@app.route("/api/v1.0/original_testing_data")
+def original_test_data():
+    conn = sqlite3.connect(insurance_info)
+
     # Create our session (link) from Python to the DB
-    # session = Session(engine)
+    test_data = conn.execute("SELECT * FROM original_test_data_vals;")
 
-    # """Return a list of passenger data including the name, age, and sex of each passenger"""
-    # # Query all passengers
-    # results = session.query(Passenger.name, Passenger.age, Passenger.sex).all()
+    """Return a list"""
+    test_data_array = []
 
-    # session.close()
+    for row in test_data:
+            test_data_array.append({'id': row[0] , 
+                                    'gender': row[1] ,
+                                    'age': row[2],
+                                    'driving license': row[3],
+                                    'region code': row[4],
+                                    'previously insured': row[5] ,
+                                    'vehicle age': row[6],
+                                    'vehicle damage': row[7],
+                                    'annual premium': row[8],
+                                    'policy sales_channel': row[9],
+                                    'vintage': row[10] })
 
-    # # Create a dictionary from the row data and append to a list of all_passengers
-    # all_passengers = []
-    # for name, age, sex in results:
-    #     passenger_dict = {}
-    #     passenger_dict["name"] = name
-    #     passenger_dict["age"] = age
-    #     passenger_dict["sex"] = sex
-    #     all_passengers.append(passenger_dict)
+    conn.close
 
-    # return jsonify(all_passengers)
+    return jsonify(test_data_array)
 
 
 if __name__ == '__main__':
