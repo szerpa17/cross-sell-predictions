eb1b9f47384295374ad8769a0ffb72f46dc6e102
szerp <szerp002@gmail.com>
2020-11-05 23:01:10 -0500

    updated flask app with sqlite info, further troubleshooting needed

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 # import numpy as np
 
-# import sqlalchemy
-# from sqlalchemy.ext.automap import automap_base
-# from sqlalchemy.orm import Session
-# from sqlalchemy import create_engine, func
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
 
 from flask import Flask, jsonify
  
@@ -11,15 +11,16 @@ from flask import Flask, jsonify
 # #################################################
 # # Database Setup
 # #################################################
-# engine = create_engine("sqlite:///titanic.sqlite")
+engine = create_engine("Output/sqlite:///insurance_data.sqlite")
 
-# # reflect an existing database into a new model
-# Base = automap_base()
-# # reflect the tables
-# Base.prepare(engine, reflect=True)
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
 
-# # Save reference to the table
-# Passenger = Base.classes.passenger
+# Save reference to the table
+test_data = Base.classes.test_data
+orig_test_vals = Base.classes.original_test_data_vals
 
 #################################################
 # Flask Setup
@@ -36,26 +37,26 @@ def welcome():
     """List all available api routes."""
     return (
         f"Available Routes:<br/>"
-        f"/api/v1.0/names<br/>"
+        f"/api/v1.0/testing_data<br/>"
         f"/api/v1.0/passengers"
     )
 
 
-# @app.route("/api/v1.0/names")
-# def names():
+@app.route("/api/v1.0/testing_data")
+def names():
     # Create our session (link) from Python to the DB
-    # session = Session(engine)
+    session = Session(engine)
 
     """Return a list of all passenger names"""
-    # Query all passengers
-    # results = session.query(Passenger.name).all()
+    # Query all test_data
+    results = session.query(test_data.name).all()
 
-    # session.close()
+    session.close()
 
     # # Convert list of tuples into normal list
-    # all_names = list(np.ravel(results))
+    all_test_data = list(np.ravel(results))
 
-    # return jsonify(all_names)
+    return jsonify(all_test_data)
 
 
 # @app.route("/api/v1.0/passengers")
