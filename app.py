fb71c02dbc74c255f98f165d3da2528fe54f9c21
szerp <szerp002@gmail.com>
2020-11-08 20:00:47 -0500

    created user prediction route

--- app.py
+++ app.py
@@ -174,6 +174,64 @@ def customer_prediction(customer_id):
         return {'ID Value Error': 'ID not found in test data',
                 'Next Steps': 'Please enter an ID value that ranges from 381110 to 508146'}
 
+@app.route("/api/v1.0/user_prediction/<val_1>-<val_2>-<val_3>-<val_4>-<val_5>-<val_6>-<val_7>-<val_8>-<val_9>-<val_10>")
+def user_prediction(val_1, val_2, val_3, val_4, val_5, val_6, val_7, val_8, val_9, val_10):
+    # Transform input to int
+    try:
+        gender = int(val_1)
+        age = int(val_2)
+        driving_license = int(val_3)
+        region_code = float(val_4)
+        previously_insured = int(val_5)
+        vehicle_age = int(val_6)
+        vehicle_damage = int(val_7)
+        annual_premium = float(val_8)
+        policy_sales_channel = float(val_9)
+        vintage = int(val_10)
+
+        """Create a list"""
+        user_test_data = []
+
+        user_test_data = [gender,
+                    age,
+                    driving_license,
+                    region_code,
+                    previously_insured,
+                    vehicle_age,
+                    vehicle_damage,
+                    annual_premium,
+                    policy_sales_channel,
+                    vintage]
+
+        # Load the model from the file
+        knn_from_joblib = joblib.load('recommender_model.pkl')
+
+        x_test = user_test_data
+
+        # Use the loaded model to make predictions
+        prediction = knn_from_joblib.predict([x_test])
+
+        result = ''
+        if (prediction[0]).tolist() == 0:
+            result = 'Customer not interested'
+        else:
+            result = 'Customer is interested'
+
+        return {'prediction': result}
+
+    except ValueError:
+        return {'Data type entry error': 'Values entered were not recognized',
+                'Accepted values': { 'gender' : 'Male: 0, Female: 1',
+                                    'age' : "Number value representing the customer's age",
+                                    'drivers license' : 'Customer does not have DL: 0, Customer already has DL: 1',
+                                    'region code' : "Number value representing the customer's region" ,
+                                    'previously insured' : "Customer doesn't have Car Insurance: 0, Customer already has Car Insurance: 1",
+                                    'vehicle age' : "Number value representing the customer's car age",
+                                    'vehicle damage' : "Customer didn't get his/her vehicle damaged in the past: 0, Customer got his/her vehicle damaged in the past.: 1",
+                                    'annual premium' : "The amount customer needs to pay as premium in the year",
+                                    'policy sales channel' : "Anonymized Code for the channel of outreaching to the customer ie. Different Agents, Over Mail, Over Phone, In Person, etc.",
+                                    'vintage' : 'Number of Days customer has been associated with the company'}}
+
 
 if __name__ == '__main__':
     app.run(debug=True)
