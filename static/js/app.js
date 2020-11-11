a4d14d96dfc27942edc1fc75d85e561df2ef96e8
szerp <szerp002@gmail.com>
2020-11-10 22:33:00 -0500

    updated variable names

--- static/js/app.js
+++ static/js/app.js
@@ -4,13 +4,13 @@ function init() {
     d3.json("/api/v1.0/testing_data").then((data) => {
 
         // Iterate through data
-        var id_list = data.map(function(customer) {
+        var idList = data.map(function(customer) {
             // Collect all customer ID values to populate the drop down
             return customer.id;
         })
         
         // Slice out 50 ID values for site efficiency
-        idList20Vals = id_list.slice(0,50);
+        idList20Vals = idList.slice(0,50);
 
         // Use D3 to select the location of the drop down values
         let dropdownMenu = d3.select("#customer_ID");
