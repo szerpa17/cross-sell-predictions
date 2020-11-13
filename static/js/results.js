050c532676602d03fabeb9223693d1c08da06f16
szerp <szerp002@gmail.com>
2020-11-12 23:53:36 -0500

    updated results.js to clear imgBox content

--- static/js/results.js
+++ static/js/results.js
@@ -5,10 +5,12 @@ function predict(selectedID) {
         let result = Object.values([data.prediction]);
 
         // Populate demographic info box
-        let box = d3.selectAll("#customer_results");
-
+        let box = d3.select("#customer_results");
+        let imgBox = d3.select("#rImage")
+        
         // Clear box content
         box.html("");
+        imgBox.html("");
 
         // Conditional to populate image
         if (result[0] = "Customer not interested") {
@@ -16,16 +18,16 @@ function predict(selectedID) {
             // Append customer details items to the box
             box.append("p")
                 .text(`${result[0]}`)
-            box.append("div")
+            imgBox.append("div")
                 .html('<img src="https://github.com/szerpa17/cross-sell-predictions/blob/main/images/prediction1.PNG?raw=true" alt="">');
 
         }
-        
+
         else {
             box.append("p")
                 .text(`${result[0]}`)
             // console.log(;`${result[0]}`);
-            box.append("div")
+            imgBox.append("div")
                 .html('<img src="https://github.com/szerpa17/cross-sell-predictions/blob/main/images/prediction2.PNG?raw=true" alt="">')
 
         }
