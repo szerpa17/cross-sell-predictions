d19d2389f0afc37e2fe5c5ebd0fd105f3748ab02
szerp <szerp002@gmail.com>
2020-11-12 23:43:51 -0500

    updated comments on results.js

--- static/js/results.js
+++ static/js/results.js
@@ -1,12 +1,8 @@
 // Initialize the page with a default plot
 function predict(selectedID) {
 
-    // let selectedID = d3.select("#customer_ID").node().value;
-    // console.log(selectedID);
-
     d3.json("/api/v1.0/prediction/" + selectedID).then((data) => {
         let result = Object.values([data.prediction]);
-        console.log(result[0])
 
         // Populate demographic info box
         let box = d3.selectAll("#customer_results");
@@ -16,29 +12,23 @@ function predict(selectedID) {
 
         // Conditional to populate image
         if (result[0] = "Customer not interested") {
+
             // Append customer details items to the box
             box.append("p")
                 .text(`${result[0]}`)
             box.append("div")
                 .html('<img src="https://github.com/szerpa17/cross-sell-predictions/blob/main/images/prediction1.PNG?raw=true" alt="">');
-                // .attr('class', "border-radius: 20px")
-                // .style("border-radius", "8px");
+
         }
+        
         else {
             box.append("p")
                 .text(`${result[0]}`)
             // console.log(;`${result[0]}`);
             box.append("div")
                 .html('<img src="https://github.com/szerpa17/cross-sell-predictions/blob/main/images/prediction2.PNG?raw=true" alt="">')
-                // .attr('class', "border-radius: 20px")
-                // .style("border-radius", "8px");
+
         }
-
-
     });
 }
 
-
-
-// Load init()
-init();
