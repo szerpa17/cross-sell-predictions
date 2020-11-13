6d6fba232d401bc8a08a48342a085ec839c74dd4
szerp <szerp002@gmail.com>
2020-11-12 23:34:33 -0500

    added conditional script to update the image type based on the prediction result

--- static/js/results.js
+++ static/js/results.js
@@ -3,8 +3,8 @@ function predict(selectedID) {
 
     // let selectedID = d3.select("#customer_ID").node().value;
     // console.log(selectedID);
-    
-    d3.json("/api/v1.0/prediction/"+selectedID).then((data) => {
+
+    d3.json("/api/v1.0/prediction/" + selectedID).then((data) => {
         let result = Object.values([data.prediction]);
         console.log(result[0])
 
@@ -14,15 +14,28 @@ function predict(selectedID) {
         // Clear box content
         box.html("");
 
-        // Append customer details items to the box
-        box.append("p")
-            .text(`${result[0]}`)
+        // Conditional to populate image
+        if (result = "Customer not interested") {
+            // Append customer details items to the box
+            box.append("p")
+                .text(`${result[0]}`)
             // console.log(;`${result[0]}`);
-        box.append("div")
-            .html('<img src="https://github.com/szerpa17/cross-sell-predictions/blob/main/images/prediction1.PNG?raw=true" alt="">')
-            // .attr('class', "border-radius: 20px")
-            .style("border-radius", "8px");
-    
+            box.append("div")
+                .html('<img src="https://github.com/szerpa17/cross-sell-predictions/blob/main/images/prediction1.PNG?raw=true" alt="">')
+                // .attr('class', "border-radius: 20px")
+                .style("border-radius", "8px");
+        }
+        else {
+            box.append("p")
+                .text(`${result[0]}`)
+            // console.log(;`${result[0]}`);
+            box.append("div")
+                .html('<img src="https://github.com/szerpa17/cross-sell-predictions/blob/main/images/prediction2.PNG?raw=true" alt="">')
+                // .attr('class', "border-radius: 20px")
+                .style("border-radius", "8px");
+        }
+
+
     });
 }
 
