c9ffb05aa2732fb27309dd6dda6b94a85729df56
szerp <szerp002@gmail.com>
2020-12-01 23:55:03 -0500

    created load.js file

---
+++ static/js/load.js
@@ -0,0 +1,8 @@
+
+// if (document.readyState == "complete") {
+//     alert("Your page is loaded");
+// }else{
+//     window.addEventListener("load", function() {
+//         alert("Your page is loaded");
+//     }, false);
+// }
