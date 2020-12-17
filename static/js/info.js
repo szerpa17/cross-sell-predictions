e6cf1827237abece634aabe212d94e429b31c960
szerp <szerp002@gmail.com>
2020-12-16 18:20:48 -1000

    updated populateInfo function to make use of the original_customer_data api route in info.js

--- static/js/info.js
+++ static/js/info.js
@@ -2,42 +2,42 @@
 // Function to capitalize text for labels (ignoring words under the length of 4)
 function capitalizeEachWord(str) {
     // Conditional to check if the string has any wite spaces
-    if (/\s/.test(str)){
+    if (/\s/.test(str)) {
         // Return each word in the string
-        return str.replace(/\w\S*/g, function(txt) {
+        return str.replace(/\w\S*/g, function (txt) {
             // If The word is more than 3 characters long, capitalize the first letter
-            if (txt.length >3) {
-            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-        }
+            if (txt.length > 3 ) {
+                return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+            }
             // Else do not capitalize
             else {
                 return txt;
             }
-            });
+        });
     }
     // If the string only has a single word, regardless of the length of the word - capitalize the first letter.
     else {
-        return str.charAt(0).toUpperCase()+ str.substr(1).toLowerCase();
+        return str.charAt(0).toUpperCase() + str.substr(1).toLowerCase();
     }
 }
 
 function populateInfo(selectedID) {
+    d3.json("/api/v1.0/original_customer_data").then((data) => {
+        console.log(typeof (data));
+        console.log(data);
+        var selectedID = 1;
+        let singleCustomer = data.filter(customer => customer.id == selectedID);
+        console.log(singleCustomer);
 
-    d3.json("/api/v1.0/" + selectedID).then((data) => {
-        let singleCustomer = data;
-
-        // Populate demographic info box
+        // Populate demographic info box
         let box = d3.select("#customer_details");
-
-        // Clear box content
+        // Clear box content
         box.html("");
-
-        // Append customer details items to the box
+        // Append customer details items to the box
         for (const [key, value] of Object.entries(singleCustomer[0])) {
             box.append("p")
-                .text(`${capitalizeEachWord(key)}: ${value}`);
-            console.log(`${capitalizeEachWord(key)}: ${value}`);
+                .text(`${capitalizeEachWord(key)}: ${value}`);
+            console.log(`${capitalizeEachWord(key)}: ${value}`);
         };
-
     });
 }
