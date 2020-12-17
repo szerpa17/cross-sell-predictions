619c01c52b1973d0d212223e6db991e79d1162bd
szerp <szerp002@gmail.com>
2020-12-16 18:23:31 -1000

    updated capitalizeEachWord function in info.js

--- static/js/info.js
+++ static/js/info.js
@@ -6,7 +6,7 @@ function capitalizeEachWord(str) {
         // Return each word in the string
         return str.replace(/\w\S*/g, function (txt) {
             // If The word is more than 3 characters long, capitalize the first letter
-            if (txt.length > 3 ) {
+            if (txt.length > 3 | txt == "age") {
                 return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
             }
             // Else do not capitalize
